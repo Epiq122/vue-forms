bdb2a58cb0091d40a6fc7e8530f48c43e930f9ef
Robert Gleason <l2ob122@gmail.com>
2023-07-07 20:12:22 -0700

    began creating baseinput

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,19 @@
+<script>
+import { defineProps } from 'vue';
+export default {
+  props: defineProps({
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+  }),
+};
+</script>
+<template>
+  <label v-if="label">{{ label }}</label>
+  <input :placeholder="label" class="field" :value="modelValue" />
+</template>
